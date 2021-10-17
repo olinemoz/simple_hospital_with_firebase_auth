7b307f7d91a51890fe34a4a9faf6439a6bd0f764
olinemazumder <olinemozumder@gmail.com>
2021-10-18 01:55:15 +0600

    Firebase Configured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import {Button} from "react-bootstrap";
+import initializeFirebaseAuthentication from "./Firebase/firebase.init";
+import {getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
+
+
+initializeFirebaseAuthentication()
 
 function App() {
+    const [user, setUser] = useState({})
+    const auth = getAuth();
+    const signInUsingGoogle = () => {
+        const googleProvider = new GoogleAuthProvider();
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                console.log(result.user)
+                setUser(result.user)
+            })
+    }
+
     return (
         <div>
             <h1>Welcome All Care Hospital</h1>
-            <Button variant="secondary">Click Me</Button>
+            <Button variant="secondary" onClick={signInUsingGoogle}>Google Sign In</Button>
         </div>
     );
 }
